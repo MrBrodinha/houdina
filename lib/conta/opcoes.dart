bac40e2edd01a5ddf59f71dce8d42906e2f7ce96
Emanuel Pacheco <95493968+MrBrodinha@users.noreply.github.com>
2023-12-26 23:08:21 +0000

    Good!

--- lib/conta/opcoes.dart
+++ lib/conta/opcoes.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:houdina/Notificacoes.dart';
 
 import 'account.dart';
 
